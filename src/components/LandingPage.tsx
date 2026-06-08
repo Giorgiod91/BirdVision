6bd45ece7991ae77e01cf04f253a1cb31d77815a
Giorgiod91 <giorgio.dettmar@gmx.de>
2026-06-08 14:33:20 +0200

    adding LadingPage component

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,22 @@
+// using use client for clientside code I use for the landingPage
+'use client';
+
+
+function LandingPage(){
+	
+
+
+return (
+	<div class="flex w-1/2" >
+
+	<h1> BirdVision </h1>
+
+
+
+	</div>
+)
+
+};
+
+
+export default LandingPage;
